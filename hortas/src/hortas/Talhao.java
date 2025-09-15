cf1f19bce80d1d3255e8c8ab1b732d224e07c6c6
gonca <gonca@LAPTOP-OKGA58V4>
2025-09-15 12:27:03 +0100

    Classe Talhao

--- hortas/src/hortas/Talhao.java
+++ hortas/src/hortas/Talhao.java
@@ -1,5 +1,41 @@
 package hortas;
 
-public class Talhao {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Talhao {
+    private int id;
+    private double area; 
+    private List<Plantacao> plantacoes;
+
+    public Talhao(int id, double area) {
+        this.id = id;
+        this.area = area;
+        this.plantacoes = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public List<Plantacao> getPlantacoes() {
+        return plantacoes;
+    }
+
+    public void adicionarPlantacao(Plantacao p) {
+        plantacoes.add(p);
+    }
+
+    @Override
+    public String toString() {
+        return "Talhao{" +
+                "id=" + id +
+                ", area=" + area +
+                ", plantacoes=" + plantacoes.size() +
+                '}';
+    }
 }
