8bf469da5cc2f436162691ebb07fb6664892df0b
Guilherme Russo <47653@alunos.upt.pt>
2025-09-15 12:23:56 +0100

    1º Commit Classe Plantação

--- hortas/src/hortas/Plantacao.java
+++ hortas/src/hortas/Plantacao.java
@@ -1,5 +1,34 @@
 package hortas;
 
 public class Plantacao {
+	public String dataSementeira;
+	public String tipoCultura;
 
+	public Plantacao(String dataSementeira, String tipoCultura) {
+		this.dataSementeira = dataSementeira;
+		this.tipoCultura = tipoCultura;
+	}
+
+	public String getDataSementeira() {
+		return dataSementeira;
+	}
+
+	public void setDataSementeira(String dataSementeira) {
+		this.dataSementeira = dataSementeira;
+	}
+
+	public String getTipoCultura() {
+		return tipoCultura;
+	}
+
+	public void setTipoCultura(String tipoCultura) {
+		this.tipoCultura = tipoCultura;
+	}
+
+	@Override
+	public String toString() {
+		return "Plantacao [dataSementeira=" + dataSementeira + ", tipoCultura=" + tipoCultura + "]";
+	}
+	
+	
 }
